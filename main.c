2f4397841a971fd66a7f112d72b364003888e20f
wzh <2663976081@qq.com>
2018-10-21 16:45:39 +0800

    code

--- main.c
+++ main.c
@@ -4,27 +4,27 @@ int main()
 {
     {
         char *res = trim_string("", ' ');
-        printf("%d", compare_string(res, ""));
+        printf("%d\n", compare_string(res, ""));
         free(res);
     }
     {
         char *res = trim_string(" ", ' ');
-        printf("%d", compare_string(res, ""));
+        printf("%d\n", compare_string(res, ""));
         free(res);
     }
     {
         char *res = trim_string("  12345", ' ');
-        printf("%d", compare_string(res, "12345"));
+        printf("%d\n", compare_string(res, "12345"));
         free(res);
     }
     {
         char *res = trim_string("  12345 ", ' ');
-        printf("%d", compare_string(res, "12345"));
+        printf("%d\n", compare_string(res, "12345"));
         free(res);
     }
     {
         char *res = trim_string("12345  ", ' ');
-        printf("%d", compare_string(res, "12345"));
+        printf("%d\n", compare_string(res, "12345"));
         free(res);
     }
 }
