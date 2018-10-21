0480fc7bb6bf3ed1c9a3b4896435933ec10c7093
wuzhenhai <you@example.com>
2018-10-21 07:36:10 -0700

    code

--- main.c
+++ main.c
@@ -6,56 +6,56 @@
 #include "cstring.h"
 int main()
 {
-    //{
-    //    char *res = trim_string("", ' ');
-    //    printf("%d\n", compare_string(res, ""));
-    //    free(res);
-    //}
-    //{
-    //    char *res = trim_string(" ", ' ');
-    //    printf("%d\n", compare_string(res, ""));
-    //    free(res);
-    //}
-    //{
-    //    char *res = trim_string("  12345", ' ');
-    //    printf("%d\n", compare_string(res, "12345"));
-    //    free(res);
-    //}
-    //{
-    //    char *res = trim_string("  12345 ", ' ');
-    //    printf("%d\n", compare_string(res, "12345"));
-    //    free(res);
-    //}
-    //{
-    //    char *res = trim_string("12345  ", ' ');
-    //    printf("%d\n", compare_string(res, "12345"));
-    //    free(res);
-    //}
-    //{
-    //    char *res = replace_string(",1,2,3,45", ",", "xx");
-    //    printf("%d\n", compare_string(res, "xx1xx2xx3xx45"));
-    //    free(res);
-    //}
-    //{
-    //    char *res = replace_string(",1,2,3,45", ",", "");
-    //    printf("%d\n", compare_string(res, "12345"));
-    //    free(res);
-    //}
-    //{
-    //    char *res = replace_string(",1,2,3,45", "", "");
-    //    printf("%d\n", compare_string(res, ",1,2,3,45"));
-    //    free(res);
-    //}
-    //{
-    //    char *res = replace_string(" ", " ", "123");
-    //    printf("%d\n", compare_string(res, "123"));
-    //    free(res);
-    //}
-    //{
-    //    char *res = replace_string("  ", " ", "123");
-    //    printf("%d\n", compare_string(res, "123123"));
-    //    free(res);
-    //}
+    {
+        char *res = trim_string("", ' ');
+        printf("%d\n", compare_string(res, ""));
+        free(res);
+    }
+    {
+        char *res = trim_string(" ", ' ');
+        printf("%d\n", compare_string(res, ""));
+        free(res);
+    }
+    {
+        char *res = trim_string("  12345", ' ');
+        printf("%d\n", compare_string(res, "12345"));
+        free(res);
+    }
+    {
+        char *res = trim_string("  12345 ", ' ');
+        printf("%d\n", compare_string(res, "12345"));
+        free(res);
+    }
+    {
+        char *res = trim_string("12345  ", ' ');
+        printf("%d\n", compare_string(res, "12345"));
+        free(res);
+    }
+    {
+        char *res = replace_string(",1,2,3,45", ",", "xx");
+        printf("%d\n", compare_string(res, "xx1xx2xx3xx45"));
+        free(res);
+    }
+    {
+        char *res = replace_string(",1,2,3,45", ",", "");
+        printf("%d\n", compare_string(res, "12345"));
+        free(res);
+    }
+    {
+        char *res = replace_string(",1,2,3,45", "", "");
+        printf("%d\n", compare_string(res, ",1,2,3,45"));
+        free(res);
+    }
+    {
+        char *res = replace_string(" ", " ", "123");
+        printf("%d\n", compare_string(res, "123"));
+        free(res);
+    }
+    {
+        char *res = replace_string("  ", " ", "123");
+        printf("%d\n", compare_string(res, "123123"));
+        free(res);
+    }
     {
         char *res = replace_string("  ", "1234 ", "123");
         printf("%d\n", compare_string(res, "  "));
